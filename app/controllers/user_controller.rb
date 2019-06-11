73a165bda8e95ccdb42e6ff221daa80fadfb3103
kyoung90 <kenneth.young90@gmail.com>
2019-06-10 23:04:12 -0400

    Implemented json backend for user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -32,6 +32,10 @@ class UserController < ApplicationController
 
     def show
         @user = User.find(params[:id])
+        respond_to do |format|
+            format.html { render :show }
+            format.json { render json: @user}
+        end
     end 
 
     def user_params
